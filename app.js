27c20c02df1dfd69140829873b0f06f956975600
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-02-09 14:17:59 -0600

    updated js with new page-template and index.html

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const profileDataArgs = process.argv.splice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('==================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+    console.log("Portfolio complete! Check out index.html to see results!")
+});
