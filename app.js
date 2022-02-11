63026f1e7e8cee2707d9db40f95772857f2ed1d7
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-02-10 20:07:07 -0600

    added inquirer pack and name question field

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            }
+        ])
+        .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
-    console.log("Portfolio complete! Check out index.html to see results!")
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+//     console.log("Portfolio complete! Check out index.html to see results!")
+// });
